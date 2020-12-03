0e412df44d19b83e575cc60815b93d105a1d4dc9
Neo <a.moh.sultan@gmail.com>
2020-12-03 17:25:31 +0200

    Adding Lesson 1 in unit 3 - embedded C

---
+++ [Unit 3] Embedded C/Lesson 1/Inc/platform_types.h
@@ -0,0 +1,29 @@
+/*
+ * File Name: platform_types.h
+ * Author: Abdelrahman Sultan
+ */
+
+#ifndef _PLATFORM_TYPES_H_
+#define _PLATFORM_TYPES_H_
+
+typedef unsigned char 						boolean;
+
+typedef signed char 						sint8;
+typedef unsigned char 						uint8;
+typedef signed short 						sint16;
+typedef unsigned short 						uint16;
+typedef signed int 							sint32;
+typedef unsigned int 						uint32;
+typedef signed long long int 				sint64;
+typedef unsigned long long int 				uint64;
+
+typedef volatile signed char 				vsint8;
+typedef volatile unsigned char 				vuint8;
+typedef volatile signed short 				vsint16;
+typedef volatile unsigned short 			vuint16;
+typedef volatile signed int 				vsint32;
+typedef volatile unsigned int 				vuint32;
+typedef volatile signed long long int 		vsint64;
+typedef volatile unsigned long long int 	vuint64;
+
+#endif /*_PLATFORM_TYPES_H_*/
